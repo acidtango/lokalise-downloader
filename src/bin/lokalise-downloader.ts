ff2b28e25dd494213b0ece9ff45754dd7b384a63
Daniel Ramos <danielramosacosta1@gmail.com>
2022-02-07 17:36:07 +0000

    fix: add shebang to bin

--- src/bin/lokalise-downloader.ts
+++ src/bin/lokalise-downloader.ts
@@ -1,8 +1,10 @@
+#!/usr/bin/env node
+
 import { mkdirSync, writeFileSync } from "fs"
 import { downloadLocalesAsJson } from "../fetcher"
 
 const help = `
-Usage: lokalise-downloader <apiKey> <projectId> tag1,tag2,tag3`
+Usage: lokalise-downloader tag1,tag2,tag3 <apiKey> <projectId>`
 
 async function main() {
   const args = process.argv
@@ -12,9 +14,9 @@ async function main() {
     return
   }
 
-  const apiKey = process.env.LOKALISE_API_KEY || process.argv[2]
-  const projectId = process.env.LOKALISE_PROJECT_ID || process.argv[3]
-  const tagsStr = process.env.LOKALISE_PROJECT_TAGS || process.argv[4]
+  const tagsStr = process.env.LOKALISE_PROJECT_TAGS || process.argv[2]
+  const apiKey = process.env.LOKALISE_API_KEY || process.argv[3]
+  const projectId = process.env.LOKALISE_PROJECT_ID || process.argv[4]
 
   if (!apiKey) throw new Error("Missing apiKey parameter")
   if (!projectId) throw new Error("Missing projectId parameter")
